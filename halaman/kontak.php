6b9bae06346ad9d942631e4a60a9c09ddb603904
FajarAfrizal <fajarmuhammadafrizal@smkwikrama.sch.id>
2022-02-17 22:43:34 +0700

    update

--- halaman/kontak.php
+++ halaman/kontak.php
@@ -5,13 +5,14 @@
 	<a href="https://www.facebook.com/profile.php?id=100031569450410"
 	target="_blank"
 	rel="noreferrer">
-	<img src="facebook.jpg" alt="icon"/>
+	<img src="facebook.jpg" alt="icon">
     </a>
 	<a href="https://www.Instagram.com/fajarafrizal_" target="_blank" rel="noreferrer">
-		<img src="instagram.jpg" alt="icon"/>
+		<img src="instagram.jpg" alt="icon">
     </a>
 	<a href="https://wa.me/6285157261663" target="_blank" rel ="noreferrer">
-		<img src="whatsapp.jpg" alt="icon"/>
+		<img src="facebook.jpg" alt="icon">
+    </a>
 
 	
     </div>
